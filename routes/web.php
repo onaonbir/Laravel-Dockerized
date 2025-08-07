97e4e6034a8b8d45f16645308a4d8268bb6e565b
bariskanberkay <berkaybariskan0208@gmail.com>
2025-08-07 14:29:46 +0300

    minor

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-route', function () {
+    return response()->json([
+        'message' => 'Laravel is working!',
+        'app_url' => config('app.url'),
+        'request' => [
+            'url' => request()->url(),
+            'host' => request()->getHost(),
+            'path' => request()->path(),
+            'fullUrl' => request()->fullUrl(),
+        ],
+        'headers' => request()->headers->all()
+    ]);
+});
+
+Route::fallback(function () {
+    return response()->json([
+        'error' => '404 - Route not found',
+        'requested_path' => request()->path(),
+        'available_routes' => collect(Route::getRoutes())->map(function($route) {
+            return $route->uri();
+        })->toArray()
+    ], 404);
+});
